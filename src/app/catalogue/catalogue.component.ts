fdbb28a11b552d317572c138ad06bd537728dff8
maher <chalghemi.maher@gmail.com>
2018-06-29 11:32:04 +0100

    first commit

---
+++ src/app/catalogue/catalogue.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { ApiService } from '../services/api.service';
+@Component({
+  selector: 'app-catalogue',
+  templateUrl: './catalogue.component.html',
+  styleUrls: ['./catalogue.component.css']
+})
+export class CatalogueComponent implements OnInit {
+datas = [] ;
+  constructor(private apiService: ApiService) { }
+
+  getData() {
+    this.apiService.getData().subscribe(res => {
+      console.log(res.json().data);
+      this.datas = res.json().data;
+    });
+  }
+
+  ngOnInit() {
+
+    this.getData();
+  }
+
+}
